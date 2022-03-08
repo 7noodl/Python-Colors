68e7eab26e1466e9d94826f137205a09467170ca
noodl <91991048+noooodl@users.noreply.github.com>
2022-03-08 17:09:31 -0500

    Add files via upload

---
+++ colors.py
@@ -0,0 +1,20 @@
+from colorama import init,Back,Fore
+
+init(autoreset=True)
+
+while(True):
+    print(Fore.RED + 'hey', end='')
+    print(Fore.YELLOW + 'hey', end='')
+    print(Fore.GREEN + 'hey', end='')
+    print(Fore.BLUE + 'hey', end='')
+    print(Fore.CYAN + 'hey', end='')
+    print(Fore.MAGENTA + 'hey', end='')
+    print(Fore.WHITE + 'hey', end='')
+
+    print(Back.RED + 'hey', end='')
+    print(Back.YELLOW + 'hey', end='')
+    print(Back.GREEN + 'hey', end='')
+    print(Back.BLUE + 'hey', end='')
+    print(Back.CYAN + 'hey', end='')
+    print(Back.MAGENTA + 'hey', end='')
+    print(Back.WHITE + 'hey', end='')
